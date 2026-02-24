238e4b551af5e4f27f1d59c93ac9dd5074169df3
LauraRiquetti <laurariquetti@gmail.com>
2026-02-24 19:16:11 -0300

    fix
    Criação da migration e do model de clientes

---
+++ app/Models/Cliente.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
+class Cliente extends Model
+{
+    use HasFactory;
+
+    protected function endereco(): Attribute
+    {
+        return Attribute::make(
+            // O segundo argumento $attributes traz os outros campos
+            get: fn (mixed $value, array $attributes) => 
+                "{$attributes['estado']} {$attributes['cidade']} {$attributes['bairro']} {$attributes['rua']} {$attributes['numero']}",
+        );
+    }
+
+    protected $table = 'clientes';
+
+    protected $fillable = [
+        'nome',
+        'email',
+        'senha',
+        'data_nascimento',
+        'endereco',
+    ];
+}
