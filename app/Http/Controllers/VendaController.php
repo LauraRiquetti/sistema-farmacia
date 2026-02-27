dec185ef4813630dbfa851b7185944b9c7b93ee1
LauraRiquetti <laurariquetti@gmail.com>
2026-02-26 21:37:34 -0300

    feat

--- app/Http/Controllers/VendaController.php
+++ app/Http/Controllers/VendaController.php
@@ -12,7 +12,8 @@ class VendaController extends Controller
      */
     public function index()
     {
-        //
+        $vendas = Venda::with('usuario', 'produto')->orderByDesc('id')->get();
+        return view('vendas.index', compact('vendas'));
     }
 
     /**
@@ -20,7 +21,7 @@ class VendaController extends Controller
      */
     public function create()
     {
-        //
+        return view('vendas.create');
     }
 
     /**
@@ -28,7 +29,26 @@ class VendaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validated([
+            'usuario_id' => 'required|exists:usuarios,id',
+            'produto_id' => 'required|exists:produtos,id',
+            'valor' => 'required|decimal',
+            'pagamento' => 'required|enum',
+            'status' => 'required|enum',
+            'descricao' => 'nullable|text',
+        ]);
+
+        Venda::create([
+            'usuario_id' => $request->usuario_id,
+            'produto_id' =>$request->produto_id,
+            'valor' => $request->valor,
+            'pagamento' => $request->pagamento,
+            'status' => $request->status,
+            'descricao' => $request->descricao,
+        ]);
+
+        return redirect()->route('vendas.index')
+            ->with('sucess', 'Venda cadastrada com sucesso!');
     }
 
     /**
@@ -52,9 +72,40 @@ class VendaController extends Controller
      */
     public function update(Request $request, Venda $venda)
     {
-        //
+        $validated = $request->validate([
+            'usuario_id' => ['required', 'exists:usuarios,id'],
+            'produto_id' => ['required', 'exists:produtos,id'],
+            'valor' => ['required', 'numeric'],
+            'pagamento' => ['required', 'in:debito,credito,pix'],
+            'status' => ['required', 'in:aguardando_confirmacao,separando,saiu_para_entrega,entregue'],
+            'descricao' =>['text'],
+
+        ]);
+        $venda->update([
+
+        ]);
     }
 
+    /*public function update(Request $request, Venda $venda)
+    {
+    $validated = $request->validate([
+        'cliente_id' => ['required', 'exists:clientes,id'],
+        'produto_id' => ['required', 'exists:produtos,id'],
+        'valor' => ['required', 'numeric'],
+        'pagamento' => ['required', 'in:debito,credito,pix'],
+        'status' => ['required', 'in:aguardando_confirmacao,separando,saiu_para_entrega,entregue'],
+    ]);
+
+    $venda->update([
+        'cliente_id' => $validated['cliente_id'],
+        'produto_id' => $validated['produto_id'],
+        'valor' => $validated['valor'],
+        'pagamento' => $validated['pagamento'],
+        'status' => $validated['status'],
+    ]);
+
+    return redirect()->route('vendas.index');
+    }*/
     /**
      * Remove the specified resource from storage.
      */
