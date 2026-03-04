3febf6e74548d00751a3995d2e46eb9e1012537a
LauraRiquetti <laurariquetti@gmail.com>
2026-03-03 21:25:15 -0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,37 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('', [ProdutoController::class, 'index'])->name('produtos.index');
+
 Route::get('/produtos', [ProdutoController::class, 'index'])->name('produtos.index');
 Route::get('/produtos/create', [ProdutoController::class, 'create'])->name('produtos.create');
 Route::post('/produtos', [ProdutoController::class, 'store'])->name('produtos.store');
 Route::get('/produtos/{id}', [ProdutoController::class, 'show'])->name('produtos.show');
-Route::get('/produtos/edit', [ProdutoController::class, 'edit'])->name('produtos.edit');
+Route::get('/produtos/{id}/edit', [ProdutoController::class, 'edit'])->name('produtos.edit');
 Route::put('/produtos/{id}', [ProdutoController::class, 'update'])->name('produtos.update');
 
+
+Route::get('/usuarios', [UsuarioController::class, 'index'])->name('usuarios.index');
+Route::get('/usuarios/create', [UsuarioController::class, 'create'])->name('usuarios.create');
+Route::post('/usuarios', [UsuarioController::class, 'store'])->name('usuarios.store');
+Route::get('/usuarios/{id}', [UsuarioController::class, 'show'])->name('usuarios.show');
+Route::get('/usuarios/{id}/edit', [UsuarioController::class, 'edit'])->name('usuarios.edit');
+Route::put('/usuarios/{id}', [UsuarioController::class, 'update'])->name('usuarios.update');
+
+
+Route::get('/vendas', [VendaController::class, 'index'])->name('vendas.index');
+Route::get('/vendas/create', [VendaController::class, 'create'])->name('vendas.create');
+Route::post('/vendas', [VendaController::class, 'store'])->name('vendas.store');
+Route::get('/vendas/{id}', [VendaController::class, 'show'])->name('vendas.show');
+Route::get('/vendas/{id}/edit', [VendaController::class, 'edit'])->name('vendas.edit');
+Route::put('/vendas/{id}', [VendaController::class, 'update'])->name('vendas.update');
+
+/*Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth');
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');*/
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
