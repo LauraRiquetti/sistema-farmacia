4503a35ee93bdc5916ade3d7629b169b2c0c7d0d
0001165253-maker <0001165253@senaimgaluno.com.br>
2026-03-16 18:51:47 -0300

    Fix

--- database/migrations/2026_02_24_231534_create_usuarios_table.php
+++ database/migrations/2026_02_24_231534_create_usuarios_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('cidade');
             $table->string('estado');
             $table->integer('numero');
+            $table->string('role')->default('user')->after('numero');
             $table->string('role')->default('user');
             $table->rememberToken();
             $table->timestamps();
